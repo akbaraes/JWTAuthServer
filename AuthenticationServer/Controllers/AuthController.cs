6c673ace5057b2d27a87009a458a608c2c1b9e07
akbarjinna <m.akbar@afras.com.sa>
2019-06-10 19:55:23 +0300

    Auth controller update

--- AuthenticationServer/Controllers/AuthController.cs
+++ AuthenticationServer/Controllers/AuthController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AuthenticationServer.Controllers
 {
@@ -11,5 +15,40 @@ namespace AuthenticationServer.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+
+        public ActionResult GetAccessToken()
+        {
+            //security key
+
+            var security="Akbar First JWT Token"
+            //Symmetric security key
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(security));
+
+
+            //Signing Credentials
+
+            var signin = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //Add Claims
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
+            claims.Add(new Claim("UserId", "10"));
+
+            //Create Token
+
+            var token = new JwtSecurityToken(
+                issuer: "Akbar",
+               audience: "Aathif",
+               expires: DateTime.Now.AddHours(1),
+               signingCredentials: signin,
+               claims: claims
+                );
+            //return Token
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+
+        }
     }
 }
